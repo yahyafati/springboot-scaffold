728a885c2d3c8e8c0f1630fd43e8e173140c42dd
Yahya Fati <yfati037@gmail.com>
2024-09-02 21:40:37 +0300

    initial commit

---
+++ src/main/kotlin/com/yahyafati/springbootauthenticationscaffold/SpringBootAuthenticationScaffoldApplication.kt
@@ -0,0 +1,11 @@
+package com.yahyafati.springbootauthenticationscaffold
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootAuthenticationScaffoldApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootAuthenticationScaffoldApplication>(*args)
+}
---
+++ src/test/kotlin/com/yahyafati/springbootauthenticationscaffold/SpringBootAuthenticationScaffoldApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yahyafati.springbootauthenticationscaffold
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootAuthenticationScaffoldApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
