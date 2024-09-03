3cb4aac8751c2f2987dd121712c175a7b6a99329
Yahya Fati <yfati037@gmail.com>
2024-09-03 17:11:15 +0300

    Added EnableSpringDataWebSupport

--- src/main/kotlin/com/yahyafati/springbootauthenticationscaffold/SpringBootAuthenticationScaffoldApplication.kt
+++ src/main/kotlin/com/yahyafati/springbootauthenticationscaffold/SpringBootAuthenticationScaffoldApplication.kt
@@ -2,8 +2,11 @@ package com.yahyafati.springbootauthenticationscaffold
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.web.config.EnableSpringDataWebSupport
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 class SpringBootAuthenticationScaffoldApplication
 
 fun main(args: Array<String>) {
